c05430fcf59a4c83bedb11de1dcac24369640cca
achernomyrdin <achernomyrdin@IMAC-202-201-PC.itf.local>
2021-06-15 18:40:10 +0500

    Daily commit

---
+++ src/lesson14/TestJMX.java
@@ -0,0 +1,12 @@
+package lesson14;
+
+import java.io.IOException;
+
+public class TestJMX {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		System.in.read();
+	}
+
+}
